6bd8d17e38d4acd5c55d59f259addc06c7cc8525
S-YahliGitzi <212911457@idf.il>
2025-01-11 18:08:44 +0200

    add firebase config

--- app/src/main/java/com/example/platepals/MainActivity.kt
+++ app/src/main/java/com/example/platepals/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.platepals
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.platepals.ui.theme.PlatePalsTheme
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +30,26 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        val db = Firebase.firestore
+
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815,
+        )
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("TAG", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("TAG", "Error adding document", e)
+            }
+
     }
 }
 
