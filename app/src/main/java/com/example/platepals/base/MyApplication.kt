b84f14a260809e1900c500b54c23f223ead936d1
inbal <inbalbarak123@gmail.com>
2025-01-11 13:10:22 +0200

    add local db

---
+++ app/src/main/java/com/example/platepals/base/MyApplication.kt
@@ -0,0 +1,15 @@
+package com.example.platepals.base
+
+import android.app.Application
+import android.content.Context
+
+class MyApplication: Application() {
+    object Globals {
+        var context: Context? =null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Globals.context=applicationContext
+    }
+}
