c2ce151d898edf0e584723eac82974f77d64d488
S-YahliGitzi <212911457@idf.il>
2025-02-23 15:29:23 +0200

    convert home activity to fragment

--- app/src/main/java/com/example/platepals/HomeFragment.kt
+++ app/src/main/java/com/example/platepals/HomeFragment.kt
@@ -1,68 +1,67 @@
 package com.example.platepals
+
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.platepals.model.Model
 import com.example.platepals.model.Post
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 
-class HomeActivity : AppCompatActivity() {
+class HomeFragment : Fragment() {
     private val selectedTagIds = mutableSetOf<String>()
     private var selectedSort = R.id.topButton
     private val tagViews = mutableListOf<TextView>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
 
-        loadTags()
-        selectSort()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadTags(view)
+        selectSort(view)
         loadFilteredPosts()
 
-        val topButton: Button = findViewById(R.id.topButton);
-        val newButton: Button = findViewById(R.id.newButton);
+        val topButton: Button = view.findViewById(R.id.topButton)
+        val newButton: Button = view.findViewById(R.id.newButton)
 
         topButton.setOnClickListener {
             selectedSort = R.id.topButton
-            selectSort()
+            selectSort(view)
             loadFilteredPosts()
         }
 
         newButton.setOnClickListener {
             selectedSort = R.id.newButton
-            selectSort()
+            selectSort(view)
             loadFilteredPosts()
         }
 
         val auth = Firebase.auth
+        val greeting: TextView = view.findViewById(R.id.username_text)
 
-        val greeting: TextView = findViewById(R.id.username_text);
-
-        Model.shared.getUserByEmail(auth.currentUser?.email?:"") { user ->
-
-            greeting.text = "Hello, ${user?.username}"
-
+        Model.shared.getUserByEmail(auth.currentUser?.email ?: "") { user ->
+            activity?.runOnUiThread {
+                greeting.text = "Hello, ${user?.username}"
+            }
         }
     }
 
     private fun loadFilteredPosts() {
         Model.shared.getAllPosts { allPosts ->
-            runOnUiThread {
+            activity?.runOnUiThread {
                 var filteredPosts = allPosts.filter { post ->
                     selectedTagIds.isEmpty() || post.tags.any { tag -> selectedTagIds.contains(tag) }
                 }
@@ -73,8 +72,7 @@ class HomeActivity : AppCompatActivity() {
                     filteredPosts.sortedByDescending { it.rating }
                 }
 
-                Log.i("yahli", filteredPosts.toString());
-
+                Log.i("yahli", filteredPosts.toString())
                 showPostsFragment(filteredPosts)
             }
         }
@@ -82,38 +80,35 @@ class HomeActivity : AppCompatActivity() {
 
     private fun showPostsFragment(posts: List<Post>) {
         val fragment = PostsListFragment.newInstance(posts)
-        supportFragmentManager.beginTransaction()
+        parentFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainerView, fragment)
             .commit()
     }
 
-    private fun loadTags() {
-        val flow = findViewById<androidx.constraintlayout.helper.widget.Flow>(R.id.tagsFlow)
-        val mainLayout = findViewById<ConstraintLayout>(R.id.main)
+    private fun loadTags(view: View) {
+        val flow = view.findViewById<androidx.constraintlayout.helper.widget.Flow>(R.id.tagsFlow)
+        val mainLayout = view.findViewById<ConstraintLayout>(R.id.main)
 
         Model.shared.getAllTags { tags ->
-            runOnUiThread {
+            activity?.runOnUiThread {
                 val tagIds = mutableListOf<Int>()
 
                 for (tag in tags) {
                     val tagView = createTagView(tag.name, tag.id)
                     mainLayout.addView(tagView)
                     tagIds.add(tagView.id)
-                    tagViews.add(tagView)  // Add the tag view to the list
+                    tagViews.add(tagView)
                 }
 
                 flow.referencedIds = tagIds.toIntArray()
-                selectTags()  // Call selectTags after loading the tags
+                selectTags()
             }
         }
     }
 
     private fun selectTags() {
-        // Loop through the tag views in the list
         for (tagView in tagViews) {
-            val tagId = tagView.tag.toString()  // Assuming you set the tagId as the tag in the view
-
-            // Check if the tag is in the selectedTagIds set
+            val tagId = tagView.tag.toString()
             if (selectedTagIds.contains(tagId)) {
                 tagView.setBackgroundResource(R.drawable.orange_filled_rounded_text_field)
                 tagView.setTextColor(Color.WHITE)
@@ -125,10 +120,10 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun createTagView(tagName: String, tagId: String): TextView {
-        val tagView = LayoutInflater.from(this).inflate(R.layout.item_tag, null) as TextView
+        val tagView = layoutInflater.inflate(R.layout.item_tag, null) as TextView
         tagView.id = ViewCompat.generateViewId()
         tagView.text = tagName
-        tagView.tag = tagId  // Store the tag ID as a tag
+        tagView.tag = tagId
 
         tagView.setOnClickListener {
             if (selectedTagIds.contains(tagId)) {
@@ -140,16 +135,14 @@ class HomeActivity : AppCompatActivity() {
                 tagView.setBackgroundResource(R.drawable.orange_filled_rounded_text_field)
                 tagView.setTextColor(Color.WHITE)
             }
-
             loadFilteredPosts()
         }
-
         return tagView
     }
 
-    private fun selectSort() {
-        val topButton: Button = findViewById(R.id.topButton);
-        val newButton: Button = findViewById(R.id.newButton);
+    private fun selectSort(view: View) {
+        val topButton: Button = view.findViewById(R.id.topButton)
+        val newButton: Button = view.findViewById(R.id.newButton)
 
         when (selectedSort) {
             R.id.topButton -> {
